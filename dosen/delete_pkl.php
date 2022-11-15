86facea2b72aa8a372ade121e0fd79bddcf1847d
TaufikAkbar <116306650+TaufikInnova24g@users.noreply.github.com>
2022-11-15 13:27:41 +0700

    Delete dosen directory

--- dosen/delete_pkl.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include("../template/headerdosen.php");  
-
-    if(isset($_GET['iddel'])){
-        $delete = mysqli_query($db, "DELETE FROM rekap_pkl WHERE id_mahasiswa = '".$_GET['iddel']."'");
-        echo '<script>window.location="rekapPkl.php"</script>';
-    }
--- dosen/delete_skripsi.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include("../template/headerdosen.php");  
-
-    if(isset($_GET['iddel'])){
-        $delete = mysqli_query($db, "DELETE FROM rekap_skripsi WHERE id_mahasiswa = '".$_GET['iddel']."'");
-        echo '<script>window.location="rekapSkripsi.php"</script>';
-    }
