15d57782f85f7ae79e77f5c2cb2de2cb0b1234a5
F4hrul1 <117475671+F4hrul1@users.noreply.github.com>
2022-11-15 14:47:42 +0700

    Add files via upload

---
+++ dosen/ajax/ajaxskripsi.js
@@ -0,0 +1,41 @@
+//fungsi untuk membuat objek XMLHttpRequest
+function getXMLHTTPRequest(){
+	if (window.XMLHttpRequest){
+		// code for modern browsers
+		return new XMLHttpRequest();
+	}else{
+		// code for old IE browsers
+		return new ActiveXObject("Microsoft.XMLHTTP");
+	}
+}
+
+function add_skripsi(){
+	var xmlhttp = getXMLHTTPRequest();
+	//get input value
+	var nama = encodeURI(document.getElementById('nama').value);
+	var nim = encodeURI(document.getElementById('nim').value);
+	var sks = encodeURI(document.getElementById('sks').value);
+	var ipk = encodeURI(document.getElementById('ipk').value);
+    var status_kelulusan = encodeURI(document.getElementById('status_kelulusan').value);
+	var angkatan = encodeURI(document.getElementById('angkatan').value);
+	if (nama != "" && nim != "" && sks != "" && ipk !=""&& status_kelulusan != "" && angkatan != ""){
+		//set url and inner
+		var url = "./skripsi_add.php"; 
+        // alert(url);
+		var inner = "demo";
+		//set parameter and open request
+		var params = "&nama=" + nama + "&nim=" + nim + "&ipk=" + ipk + "&sks=" + sks + "&status_kelulusan=" + status_kelulusan + "&angkatan=" + angkatan;
+		xmlhttp.open("POST", url, true);
+		xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xmlhttp.onreadystatechange = function() {
+			document.getElementById(inner).innerHTML = '<img src="images/ajax_loader.png"/>';
+			if ((xmlhttp.readyState == 4) && (xmlhttp.status == 200)){
+				 document.getElementById(inner).innerHTML = xmlhttp.responseText;
+			}
+			return false;
+		}
+		xmlhttp.send(params);
+	}else{
+		alert("Tolong diisi semua!!!!");
+	}
+}
