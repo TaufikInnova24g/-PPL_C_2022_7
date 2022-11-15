48b0ef87fee8b2e35f4561e5c5bc207ac3caecd0
nawfalfudhayl <117434460+nawfalfudhayl@users.noreply.github.com>
2022-11-15 15:05:41 +0700

    Add files via upload

---
+++ dosen/verifirs.php
@@ -0,0 +1,76 @@
+<?php include("../template/headerdosen.php");
+?>
+<div id="layoutSidenav_content">
+    <main>
+        <div class="container-fluid px-4" style="width: 80%;">
+            <h3 class="mt-4 mb-4">Verifikasi Berkas IRS</h3>
+            <div class="card mb-4">
+                <div class="card-header">
+                    <i class="fas fa-table me-1"></i>
+                    <b>Belum Terverifikasi</b>
+                </div>
+                <div class="card-body">
+                    <table class="table dataTable" id="">
+                        <thead class="table-light">
+                            <tr>
+                                <th>No</th>
+                                <th>NIM</th>
+                                <th>Nama</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                                $query = "SELECT mahasiswa.NIM,mahasiswa.Nama FROM mahasiswa,irs WHERE mahasiswa.NIM = irs.NIM AND irs.status='Belum'";
+                                $result = $db->query($query);
+                                $i = 1;
+                                while ($row = $result->fetch_object()) {
+                                    echo "<tr>
+                                    <td>".$i."</td>
+                                    <td>".$row->NIM."</td>
+                                    <td>".$row->Nama."</td>
+                                    <td><a class='btn btn-primary' href='lihatirs.php'>Lihat</a></td>";
+                                    $i++;                        
+                                }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="card mb-4">
+                <div class="card-header">
+                    <i class="fas fa-table me-1"></i>
+                    <b>Sudah Terverifikasi</b>
+                </div>
+                <div class="card-body">
+                    <table class="table dataTable" id="datatablesSimple2">
+                        <thead class="table-light">
+                            <tr>
+                                <th>No</th>
+                                <th>NIM</th>
+                                <th>Nama</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                                $query = "SELECT mahasiswa.NIM,mahasiswa.Nama FROM mahasiswa,irs WHERE mahasiswa.NIM = irs.NIM AND irs.status!='Belum'";
+                                $result = $db->query($query);
+                                $i = 1;
+                                while ($row = $result->fetch_object()) {
+                                    echo "<tr>
+                                    <td>".$i."</td>
+                                    <td>".$row->NIM."</td>
+                                    <td>".$row->Nama."</td>
+                                    <td><a class='btn btn-primary' href='lihatirs.php'>Lihat</a></td>";
+                                    $i++;                        
+                                }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <?php include("../template/footeruniversal.php")?>
